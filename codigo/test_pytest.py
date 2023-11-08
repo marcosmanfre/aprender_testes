ed665632b74b7d65ec8e0776b583ccd7e8eedd15
Marcos Antonio Manfre Ros <marcosmanfre@hotmail.com>
2023-11-08 18:03:31 -0300

    testando

---
+++ codigo/test_pytest.py
@@ -0,0 +1,9 @@
+from jogo import brincadeira
+
+def test_quando_brincadeira_receber_1_entao_deve_retornar_1():
+    entrada = 1
+    esperado = 1
+    resultado = brincadeira(entrada)
+    assert resultado == esperado
+
+    assert(brincadeira(1) == 1)
