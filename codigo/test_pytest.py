e5ee641ef36559f65db74819f24fd1fb9f87f472
Marcos Antonio Manfre Ros <marcosmanfre@hotmail.com>
2023-11-08 18:51:00 -0300

    testando

--- codigo/test_pytest.py
+++ codigo/test_pytest.py
@@ -1,4 +1,6 @@
 from codigo.jogo import brincadeira
+from pytest import mark
+
 
 def test_quando_brincadeira_receber_1_entao_deve_retornar_1():
     entrada = 1
@@ -8,5 +10,7 @@ def test_quando_brincadeira_receber_1_entao_deve_retornar_1():
 
     assert(brincadeira(1) == 1)
 
+
+@mark.testenew
 def test_quando_brincadeira_receber_2_entao_deve_retornar_2():
     assert brincadeira(2) == 2
