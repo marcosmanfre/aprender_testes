539e6262e1d9a59da8cc85f73e96cc06b54ea04f
Marcos Antonio Manfre Ros <marcosmanfre@hotmail.com>
2023-11-09 16:54:51 -0300

    testando

--- tests/test_my_functions.py
+++ tests/test_my_functions.py
@@ -1,6 +1,17 @@
 import pytest
 import source.my_functions as my_functions
 
+
 def test_add():
     result = my_functions.add(number_one=1, number_two=4)
-    assert result == 6
+    assert result == 5
+
+
+def test_divide():
+    result = my_functions.divide(number_one=10, number_two=5)
+    assert result == 2
+
+
+def test_divide_by_0():
+    result = my_functions.divide(number_one=10, number_two=0)
+    assert True
